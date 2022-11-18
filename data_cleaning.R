7c653ae8f3eb68f82ea6f9af6f0e193952f96602
Mia <70609303+miaminkin@users.noreply.github.com>
2022-11-18 14:03:26 -0500

    Data Cleaning/Uploading

---
+++ data_cleaning.R
@@ -0,0 +1,33 @@
+#Final Project
+
+#Packages
+library(tidyverse)
+library(stringr)
+
+#Reading in data
+air_quality<-read_csv("corn_counties_aq.csv")
+x<-air_quality$state_code
+state_abbreviations<-state.abb[match(x,state.name)
+air_quality$state_code<-c(state.abb)
+
+corn_yield<-read_csv("corn_yield.csv")
+corn_yield$state_code<-corn_yield$State
+
+drought<-read_csv("drought_means.csv")
+drought<-drought[-c(1)]
+drought$County<-c(county_names)
+county_names<-str_remove(drought$County,"County")
+
+
+#New Column With State + County
+##air_quality
+air_quality$state_county<-paste(air_quality$state_code,air_quality$County,sep=",")
+corn_yield$state_county<-paste(corn_yield$state_code,corn_yield$County,sep=",")
+drought$state_county<-paste(drought$State,drought$County,sep=",")
+
+
+#Cleaning data
+corn_harvested<-corn_yield[-c(3,4)]
+air_and_corn<-left_join(air_quality,corn_harvested,drought,
+                        
+
